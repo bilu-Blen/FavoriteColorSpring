f700b0fb7578ac97fbfc9896b9a7bf288020af0e
bilu-Blen <azblen1@gmail.com>
2018-10-24 16:55:27 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+public class User {
+    private String name;
+    private String quest;
+    private  String favColor;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuest() {
+        return quest;
+    }
+
+    public void setQuest(String quest) {
+        this.quest = quest;
+    }
+
+    public String getFavColor() {
+        return favColor;
+    }
+
+    public void setFavColor(String favColor) {
+        this.favColor = favColor;
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/algorithm.md
@@ -0,0 +1,10 @@
+Input
+The users answer to the question
+
+Process
+Display a from for the user to fill
+validate the form 
+when submitted show a confirmation page
+
+OutPut
+confirmation page
